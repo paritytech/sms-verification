28a94ee5c6c58dc188045cbd7d9eec16c8ef1d81
Jannis R <mail@jannisr.de>
2016-10-27 13:40:16 +0200

    fix contract call

--- post-to-contract.js
+++ post-to-contract.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { address } = require('config')
+const { address, owner } = require('config')
 const abi = require('./SMSVerification.abi.json')
 
 const Web3 = require('web3')
@@ -11,13 +11,18 @@ const contract = web3.eth.contract(abi).at(address)
 
 const postToContract = (number, code) =>
   new Promise((resolve, reject) => {
+    const numberHash = sha3(number)
     // The response to the challenge. Because arbitrary-length strings don't play nicely with contracts, we use `sha3(code)`.
     const token = sha3(code)
     // Will be stored inside the (public) contract, paired with `sha3(number)`.
-    const tokenHash = sha3(token)
+    const tokenHash = sha3(token, {encoding: 'hex'})
 
-    const numberHash = sha3(number)
-    contract.puzzle.sendTransaction(tokenHash, numberHash, (err, address) => {
+    if (contract.certified(numberHash))
+      return reject(new Error('This number is already verified.'))
+
+    contract.puzzle.sendTransaction(tokenHash, numberHash, {
+      from: owner
+    }, (err, address) => {
       if (err) reject(err)
       else resolve(address)
     })
